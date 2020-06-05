41716e6730098bd66bd6baf18740d467d9e706eb
sayangit03 <sayman.eye@gmail.com>
2020-06-06 01:02:04 +0530

    test

--- src/main/java/com/spring/microservices/MyController.java
+++ src/main/java/com/spring/microservices/MyController.java
@@ -1,13 +1,31 @@
 package com.spring.microservices;
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MyController {
-
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@RequestMapping("/")
 	public String redirectUrl() {
 		return "index";
+	}
+	
+	@RequestMapping("/users")
+	@ResponseBody
+	public List<Object> userService() {
+		System.out.println("Fetching details....");
+		String url = "http://userservice-env.eba-rnibmpmc.us-east-2.elasticbeanstalk.com/getRegUserDetails";
+		ResponseEntity<Object[]> regDetails = restTemplate.getForEntity(url, Object[].class);
+		return Arrays.asList(regDetails.getBody());
 	}
 }
--- src/main/java/com/spring/microservices/TestWebAppApplication.java
+++ src/main/java/com/spring/microservices/TestWebAppApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.spring.microservices")
@@ -17,6 +19,11 @@ public class TestWebAppApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(TestWebAppApplication.class);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
